f7c46208084dddb48045f3af35606d4c6aa31502
YUVRAJ AGGARWAL <87929724+1yvuraj@users.noreply.github.com>
2022-01-17 22:48:39 +0530

    Time: 25 ms (58.37%), Space: 39.5 MB (64.79%) - LeetHub

--- 712-minimum-ascii-delete-sum-for-two-strings/712-minimum-ascii-delete-sum-for-two-strings.java
+++ 712-minimum-ascii-delete-sum-for-two-strings/712-minimum-ascii-delete-sum-for-two-strings.java
@@ -1,38 +1,45 @@
 class Solution {
     public int minimumDeleteSum(String s1, String s2) {
         int [][]dp=new int[s1.length()+1][s2.length()+1];
-       return ok(s1,s2,0,0,dp); 
+       return ok(s1,s2,dp); 
     }
-    public int ok(String s1,String s2,int i,int j,int [][]dp)
+    public int ok(String s1,String s2,int [][]dp)
     {
-        if(i==s1.length()&&j==s2.length())
+        
+        for(int i = s1.length() ; i >= 0 ; i--){
+            for(int j = s2.length() ; j >= 0 ; j--){
+               if(i==s1.length()&&j==s2.length())
         {
-            return 0;
+            dp[i][j] = 0;
+                   continue;
         }
         
         int f1=Integer.MAX_VALUE;
-        if(dp[i][j]!=0)
-        {
-            return dp[i][j];
-        }
+       
         if(i<s1.length() && j<s2.length() &&s1.charAt(i)==s2.charAt(j))
         {
-           f1=Math.min(f1,ok(s1,s2,i+1,j+1,dp));
+           f1=Math.min(f1,dp[i + 1][j + 1]);
         }
         else{
             if(i<s1.length())
             {
-                f1=Math.min(f1,ok(s1,s2,i+1,j,dp)+(int)(s1.charAt(i)));
+                f1=Math.min(f1,dp[i + 1][j]+(int)(s1.charAt(i)));
                 
             }
             if(j<s2.length())
             {
-                f1=Math.min(f1,ok(s1,s2,i,j+1,dp)+(int)(s2.charAt(j)));
+                f1=Math.min(f1,dp[i ][j + 1]+(int)(s2.charAt(j)));
             }
                 
             
         }
                        
-        return dp[i][j]=f1;                 
+            dp[i][j]=f1;      
+            }
+        }
+        
+      
+        return dp[0][0];
+                    
     }
 }
