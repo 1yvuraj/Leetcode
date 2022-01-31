ecbcf1f8698704451a0d40a004a4a31e2aa1f278
YUVRAJ AGGARWAL <87929724+1yvuraj@users.noreply.github.com>
2022-01-31 17:57:57 +0530

    Time: 3 ms (98.29%), Space: 41.3 MB (12.39%) - LeetHub

---
+++ 576-out-of-boundary-paths/576-out-of-boundary-paths.java
@@ -0,0 +1,35 @@
+class Solution {
+    int c=0;
+     int mod=(int)1e9+7;
+    public int findPaths(int m, int n, int maxMove, int startRow, int startColumn) {
+       int[][][]dp=new int[m+1][n+1][maxMove+1];
+        for(int[][] c1 : dp)
+        for(int[] c2 : c1)
+        Arrays.fill(c2,-1);
+         
+        return ok(m,n,maxMove,startRow,startColumn,dp);
+    }
+    public int ok(int m,int n,int maxMove,int startRow,int startColumn,int[][][]dp)
+    {
+        if(startRow<0 || startColumn<0|| startRow>=m || startColumn>=n)
+        {
+            
+            return 1;
+        }
+        if(maxMove==0)
+        {
+            return 0;
+        }
+        if(dp[startRow][startColumn][maxMove]!=-1)
+        {
+            return dp[startRow][startColumn][maxMove];
+        }
+     int ans = 0;
+          ans =  (ans + ok(m,n,maxMove-1,startRow+1,startColumn,dp))%mod;
+           ans =(ans + ok(m,n,maxMove-1,startRow-1,startColumn,dp))%mod;
+           ans = (ans + ok(m,n,maxMove-1,startRow,startColumn+1,dp))%mod;
+           ans =(ans + ok(m,n,maxMove-1,startRow,startColumn-1,dp))%mod;
+        
+        return dp[startRow][startColumn][maxMove]= ans % mod;
+    }
+}
