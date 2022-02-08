ae4dfe547aa227932b24d07093309d4542c5f205
YUVRAJ AGGARWAL <87929724+1yvuraj@users.noreply.github.com>
2022-02-08 21:33:32 +0530

    Time: 3 ms (46.67%), Space: 44 MB (5.41%) - LeetHub

--- 662-maximum-width-of-binary-tree/662-maximum-width-of-binary-tree.java
+++ 662-maximum-width-of-binary-tree/662-maximum-width-of-binary-tree.java
@@ -13,73 +13,74 @@
  *     }
  * }
  */
-// class Solution {
-
-//     public static class pair {
-//         TreeNode node;
-//         int idx;
-
-//         pair(TreeNode node, int idx) {
-//             this.node = node;
-//             this.idx = idx;
-//         }
-//     }
-
-//      static int max = 0;
-
-//     public int widthOfBinaryTree(TreeNode root) {
-//         LinkedList<pair> q = new LinkedList<>();
-//         q.addLast(new pair(root, 0));
-//         while (q.size() != 0) {
-//             int size = q.size();
-//             int lm = q.getFirst().idx;
-//             int rm = q.getFirst().idx;
-//             while (size-- > 0) {
-//                 pair r = q.removeFirst();
-//                 rm = r.idx;
-//                 if (r.node.left != null) {
-//                     q.add(new pair(r.node.left, 2 * r.idx + 1));
-//                 }
-//                 if (r.node.right != null) {
-//                     q.add(new pair(r.node.right, 2 * r.idx + 2));
-//                 }
-//             }
-//             max = Math.max(max, (rm) - (lm) + 1);
-//         }
-//         return max;
-//     }
-// }
 class Solution {
-    public class pair {
-        TreeNode node = null;
-        long w = 0;
-        pair(TreeNode node, long w) {
+
+    public static class pair {
+        TreeNode node;
+        int idx;
+
+        pair(TreeNode node, int idx) {
             this.node = node;
-            this.w = w;
+            this.idx = idx;
         }
     }
+
+     // static int max = 0;
+
     public int widthOfBinaryTree(TreeNode root) {
-        LinkedList<pair> que = new LinkedList<>();
-        que.addLast(new pair(root, 0));
-        int ans = 0;
-        while (que.size() != 0) {
-            int size = que.size();
-            long fi = que.getFirst().w;
-            long li = que.getFirst().w;
+        LinkedList<pair> q = new LinkedList<>();
+        int ans=0;
+        q.addLast(new pair(root, 0));
+        while (q.size() != 0) {
+            int size = q.size();
+            int lm = q.getFirst().idx;
+            int rm = q.getFirst().idx;
             while (size-- > 0) {
-                pair p = que.removeFirst();
-                TreeNode node = p.node;
-                long w = p.w;
-                li = w;
-                if (node.left != null){
-                    que.addLast(new pair(node.left, 2 * w+1));
+                pair r = q.removeFirst();
+                rm = r.idx;
+                if (r.node.left != null) {
+                    q.add(new pair(r.node.left, 2 * r.idx + 1));
                 }
-                if (node.right != null){
-                    que.addLast(new pair(node.right, 2 * w + 2));
+                if (r.node.right != null) {
+                    q.add(new pair(r.node.right, 2 * r.idx + 2));
                 }
             }
-            ans = Math.max(ans, (int) (li - fi + 1));
+            ans = Math.max(ans, (rm) - (lm) + 1);
         }
         return ans;
     }
 }
+// class Solution {
+//     public class pair {
+//         TreeNode node = null;
+//         long w = 0;
+//         pair(TreeNode node, long w) {
+//             this.node = node;
+//             this.w = w;
+//         }
+//     }
+//     public int widthOfBinaryTree(TreeNode root) {
+//         LinkedList<pair> que = new LinkedList<>();
+//         que.addLast(new pair(root, 0));
+//         int ans = 0;
+//         while (que.size() != 0) {
+//             int size = que.size();
+//             long fi = que.getFirst().w;
+//             long li = que.getFirst().w;
+//             while (size-- > 0) {
+//                 pair p = que.removeFirst();
+//                 TreeNode node = p.node;
+//                 long w = p.w;
+//                 li = w;
+//                 if (node.left != null){
+//                     que.addLast(new pair(node.left, 2 * w+1));
+//                 }
+//                 if (node.right != null){
+//                     que.addLast(new pair(node.right, 2 * w + 2));
+//                 }
+//             }
+//             ans = Math.max(ans, (int) (li - fi + 1));
+//         }
+//         return ans;
+//     }
+// }
