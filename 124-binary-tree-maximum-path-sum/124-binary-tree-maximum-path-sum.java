3100347c6e09a9c2a2efeb3e2611872a4863cfbd
YUVRAJ AGGARWAL <87929724+1yvuraj@users.noreply.github.com>
2022-02-10 13:38:00 +0530

    Time: 1 ms (68.49%), Space: 43.4 MB (68.12%) - LeetHub

---
+++ 124-binary-tree-maximum-path-sum/124-binary-tree-maximum-path-sum.java
@@ -0,0 +1,52 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+     public static class pair {
+        int RtNpath = -(int)1e9;
+        int NtNpath = -(int)1e9;
+    }
+    public int maxPathSum(TreeNode root) {
+        if(root==null)return 0;
+        if(root.left==null && root.right==null )return root.val;
+        
+        return maxPathSu(root).NtNpath;
+    }
+
+    
+    public static int max(int... arr)
+    {
+        int maxsum=arr[0];
+        for(int e:arr)
+        {
+            maxsum=Math.max(e,maxsum);
+        }
+        return maxsum;
+    }
+
+    public static pair maxPathSu(TreeNode root) {
+        if (root == null) return new pair();
+       
+        pair l = maxPathSu(root.left);
+        pair r = maxPathSu(root.right);
+        pair ans = new pair();
+        int RtNpath=Math.max(l.RtNpath,r.RtNpath)+root.val;
+        int NtNpath=max(l.NtNpath,r.NtNpath,l.RtNpath+r.RtNpath+root.val,root.val,RtNpath);
+        ans.RtNpath=Math.max(root.val,RtNpath);
+        ans.NtNpath=NtNpath;
+        
+        return ans;
+    }
+}
