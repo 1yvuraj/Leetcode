a8e58baf9fefea0affd1a9e46614489ef96b9198
YUVRAJ AGGARWAL <87929724+1yvuraj@users.noreply.github.com>
2022-02-10 20:11:04 +0530

    Time: 3 ms (45.06%), Space: 46.5 MB (6.32%) - LeetHub

--- 95-unique-binary-search-trees-ii/95-unique-binary-search-trees-ii.java
+++ 95-unique-binary-search-trees-ii/95-unique-binary-search-trees-ii.java
@@ -15,27 +15,23 @@
  */
 class Solution {
     public List<TreeNode> generateTrees(int n) {
-        return help(1,n);
+        return help(1, n);
     }
-    public List<TreeNode> help(int si,int e) {
-        if(si>e)
-        {
-           List<TreeNode>a=new ArrayList<>();
+    public List<TreeNode> help(int si, int e) {
+        if (si > e) {
+            List<TreeNode> a = new ArrayList<>();
             a.add(null);
             return a;
         }
-         List<TreeNode>ans=new ArrayList<>();
-        for(int i=si;i<=e;i++)
-        {
-            List<TreeNode>l=help(si,i-1);
-            List<TreeNode>r=help(i+1,e);
-            for(TreeNode Left:l)
-            {
-                for(TreeNode right:r)
-                {
-                  TreeNode root=new TreeNode(i);
-                  root.left=Left;
-                    root.right=right;
+        List<TreeNode> ans = new ArrayList<>();
+        for (int i = si; i <= e; i++) {
+            List<TreeNode> l = help(si, i - 1);
+            List<TreeNode> r = help(i + 1, e);
+            for (TreeNode Left : l) {
+                for (TreeNode right : r) {
+                    TreeNode root = new TreeNode(i);
+                    root.left = Left;
+                    root.right = right;
                     ans.add(root);
                 }
             }
