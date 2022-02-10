8be4970c37af1ec7e3c4c8f19ca6b1e52a5b1bb4
YUVRAJ AGGARWAL <87929724+1yvuraj@users.noreply.github.com>
2022-02-10 16:47:47 +0530

    Time: 10 ms (49.76%), Space: 47 MB (16.45%) - LeetHub

--- 236-lowest-common-ancestor-of-a-binary-tree/236-lowest-common-ancestor-of-a-binary-tree.java
+++ 236-lowest-common-ancestor-of-a-binary-tree/236-lowest-common-ancestor-of-a-binary-tree.java
@@ -8,42 +8,25 @@
  * }
  */
 class Solution {
+    TreeNode node=null;
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-       ArrayList<TreeNode> ans = nodeToRootPath(root, p);
-        ArrayList<TreeNode> ans1 = nodeToRootPath(root, q);
-        int i = ans.size() - 1;
-        int j = ans1.size() - 1;
-        while (i >= 0 && j >= 0 && ans.get(i) == ans1.get(j)) {
-            i--;
-            j--;
-        }
-        
-        return ans.get(i+1);
-    } 
-    
-    
-    public  ArrayList<TreeNode> nodeToRootPath(TreeNode node, TreeNode data) {
-        if (node == null) {
-            return new ArrayList<>();
-        }
-        if (node == data) {
-            ArrayList<TreeNode> root = new ArrayList<>();
-            root.add(node);
-            return root;
-        }
-        ArrayList<TreeNode> l = nodeToRootPath(node.left, data);
-        if (l.size() > 0) {
-            l.add(node);
-            return l;
-        }
-        ArrayList<TreeNode> r = nodeToRootPath(node.right, data);
-        if (r.size() > 0) {
-            r.add(node);
-            return r;
-        }
-        return new ArrayList<>();
+        lowest(root,p,q);
+        return node;
     }
-
-    
+    public boolean lowest(TreeNode root, TreeNode p, TreeNode q) {
+        if(root==null)return false;
+        boolean self=(root==p)||(root==q);
+        boolean l=lowest(root.left,p,q);
+        if(node!=null)return true;
+        boolean r=lowest(root.right,p,q);
+        if(node!=null)return true;
+        
+       if(self && l||r&& self||l&&r)
+       {
+           node=root;
+       }
+        return self||l||r;
+        
+        
+    }
 }
-
