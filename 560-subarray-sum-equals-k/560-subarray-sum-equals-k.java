0b3994d1c34d9b37d0753c2b2aaec07da082624c
YUVRAJ AGGARWAL <87929724+1yvuraj@users.noreply.github.com>
2022-02-10 10:10:45 +0530

    Time: 58 ms (25.75%), Space: 62.2 MB (7.61%) - LeetHub

--- 560-subarray-sum-equals-k/560-subarray-sum-equals-k.java
+++ 560-subarray-sum-equals-k/560-subarray-sum-equals-k.java
@@ -1,21 +1,40 @@
-class Solution {
-    public int subarraySum(int[] nums, int k) {
-        int sum=0;
-        int c=0;
-        for(int i=0;i<nums.length;i++)
-        {
-            sum=0;
-            for(int j=i;j<nums.length;j++)
-            {
-                sum+=nums[j];
-                if(sum==k)
-                {
-                    ++c;
+// class Solution {
+//     public int subarraySum(int[] nums, int k) {
+//         int sum=0;
+//         int c=0;
+//         for(int i=0;i<nums.length;i++)
+//         {
+//             sum=0;
+//             for(int j=i;j<nums.length;j++)
+//             {
+//                 sum+=nums[j];
+//                 if(sum==k)
+//                 {
+//                     ++c;
                    
-                }
-            }
-        }
-        return c;
+                   
+//                 }
+//             }
+//         }
+//         return c;
         
-    }
-}
+//     }
+// }
+class Solution {
+ 	public int subarraySum(int[] nums, int k) {
+
+ 		HashMap<Integer, Integer> arr_sums = new HashMap();
+ 		
+ 		int sum = 0, count = 0;
+ 		
+ 		for(int i = 0; i < nums.length; i++) {
+            arr_sums.put(sum, arr_sums.getOrDefault(sum, 0) + 1);
+ 			sum += nums[i];
+ 			if(arr_sums.containsKey(sum - k)) {
+ 				count += arr_sums.get(sum - k);
+ 			}
+ 			
+ 		}
+ 		return count;
+ 	}
+ }
