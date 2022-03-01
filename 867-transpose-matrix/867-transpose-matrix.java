41a49387659f4670933830ea0af7c260963ec54c
YUVRAJ AGGARWAL <87929724+1yvuraj@users.noreply.github.com>
2022-03-01 22:49:19 +0530

    Time: 1 ms (65.33%), Space: 48.8 MB (10.91%) - LeetHub

---
+++ 867-transpose-matrix/867-transpose-matrix.java
@@ -0,0 +1,15 @@
+
+class Solution {
+    public int[][] transpose(int[][] matrix) {
+            int[][]ans=new int[matrix[0].length][matrix.length];
+         for(int i=0;i<ans[0].length;i++){
+            for(int j=0;j<ans.length;j++)
+            {
+                ans[j][i]=matrix[i][j];
+                
+            }
+         }
+        
+        return ans;
+    }
+}
