ef3aa2f5635b10e6bc7ebf3778737f93cd767ad6
YUVRAJ AGGARWAL <87929724+1yvuraj@users.noreply.github.com>
2022-03-01 20:49:41 +0530

    Time: 171 ms (5.00%), Space: 55.5 MB (5.23%) - LeetHub

--- 283-move-zeroes/283-move-zeroes.java
+++ 283-move-zeroes/283-move-zeroes.java
@@ -1,35 +1,4 @@
-//class Solution {
-//     public void moveZeroes(int[] nums) {
-//         int y=0;
-//         for(int i=0;i<nums.length;i++)
-//         {
-//             if(nums[i]!=0)
-//             {
-                
-                
-//                 nums[y]=nums[i];
-//                 ++y;
-                
-//             }
-            
-           
-            
-//         }
-//        while(y<nums.length)
-//        {
-//            nums[y]=0;
-//            ++y;
-//        }
-//        for(int i=0;i<nums.length;i++)
-//            System.out.print(nums[i]);
-     
-    
 
-        
-//     }
-    
-// }
-// }
 class Solution {
     public void moveZeroes(int[] nums) {
     
