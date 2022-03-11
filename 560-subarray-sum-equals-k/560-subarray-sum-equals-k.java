8059323f9ef26061d9a7e81fd5ffdc62ba874a29
YUVRAJ AGGARWAL <87929724+1yvuraj@users.noreply.github.com>
2022-03-11 22:53:43 +0530

    Time: 31 ms (68.48%), Space: 62.8 MB (8.69%) - LeetHub

--- 560-subarray-sum-equals-k/560-subarray-sum-equals-k.java
+++ 560-subarray-sum-equals-k/560-subarray-sum-equals-k.java
@@ -1,40 +1,24 @@
-// class Solution {
-//     public int subarraySum(int[] nums, int k) {
-//         int sum=0;
-//         int c=0;
-//         for(int i=0;i<nums.length;i++)
-//         {
-//             sum=0;
-//             for(int j=i;j<nums.length;j++)
-//             {
-//                 sum+=nums[j];
-//                 if(sum==k)
-//                 {
-//                     ++c;
-                   
-                   
-//                 }
-//             }
-//         }
-//         return c;
-        
-//     }
-// }
 class Solution {
- 	public int subarraySum(int[] nums, int k) {
-
- 		HashMap<Integer, Integer> arr_sums = new HashMap();
- 		
- 		int sum = 0, count = 0;
- 		
- 		for(int i = 0; i < nums.length; i++) {
-            arr_sums.put(sum, arr_sums.getOrDefault(sum, 0) + 1);
- 			sum += nums[i];
- 			if(arr_sums.containsKey(sum - k)) {
- 				count += arr_sums.get(sum - k);
- 			}
- 			
- 		}
- 		return count;
- 	}
- }
+    public int subarraySum(int[] nums, int k) {
+        int sum=0;
+        int ans=0;
+        HashMap<Integer,Integer>map=new HashMap<>();
+        map.put(sum,1);
+        for(int i=0;i<nums.length;i++){
+            sum+=nums[i];
+            if(map.containsKey(sum-k))
+            {
+                ans+=map.get(sum-k);
+            
+            }
+            if(map.containsKey(sum))
+            {
+                map.put(sum,map.get(sum)+1);
+            
+            }else{
+                map.put(sum,1);
+            }
+        }
+        return ans;
+    }
+}
