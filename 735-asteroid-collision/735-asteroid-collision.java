b01770b896f100e0e4393219aa931d6d8c5f91b3
YUVRAJ AGGARWAL <87929724+1yvuraj@users.noreply.github.com>
2022-03-12 21:34:52 +0530

    Time: 14 ms (31.45%), Space: 49.1 MB (5.67%) - LeetHub

--- 735-asteroid-collision/735-asteroid-collision.java
+++ 735-asteroid-collision/735-asteroid-collision.java
@@ -1,68 +1,68 @@
-// class Solution {
-
-//     public int[] asteroidCollision(int[] arr) {
-//         Stack<Integer> st = new Stack<>();
-
-//         for (int i = 0; i < arr.length; i++) {
-//             if (arr[i] < 0) {
-//                 while (st.size() > 0 && st.peek() < arr[i] * -1) {
-//                     st.pop();
-//                 }
-//                 if (st.size() > 0 && st.peek() == arr[i] * -1) {
-//                     st.pop();
-//                 } else if (st.size() > 0 && st.peek() >=arr[i] * -1) {
-//                     continue;
-//                 } else {
-//                     st.push(arr[i]);
-//                 }
-//             } else {
-//                 st.push(arr[i]);
-//             }
-//         }
-//         int[] ans = new int[st.size()];
-//         int idx = st.size()-1;
-//         while (st.size() > 0) {
-//             ans[idx] = st.pop();
-//             idx--;
-//         }
-//         return ans;
-//     }
-// }
-
 class Solution {
-public int[] asteroidCollision(int[] ast)
-{
 
-    Stack<Integer> st = new Stack<>();
-    int n=ast.length;
-   for(int i=0;i<n;i++)
-    {
-        if(ast[i]<=0)
-        {
-            while(!st.isEmpty() && st.peek ()> 0 && st.peek()<ast[i]*-1)
-                st.pop();
-            
-            if(!st.isEmpty() && st.peek()==ast[i]*-1)
-                st.pop();
-            
-            else if(!st.isEmpty() && st.peek() >= -1*ast[i])
-                continue;
-            else
-                st.push (ast[i]);
+    public int[] asteroidCollision(int[] arr) {
+        Stack<Integer> st = new Stack<>();
+
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] < 0) {
+                while (st.size() > 0 &&  st.peek ()> 0 && st.peek() < arr[i] * -1) {
+                    st.pop();
+                }
+                if (st.size() > 0 && st.peek() == arr[i] * -1) {
+                    st.pop();
+                } else if (st.size() > 0 && st.peek() >=arr[i] * -1) {
+                    continue;
+                } else {
+                    st.push(arr[i]);
+                }
+            } else {
+                st.push(arr[i]);
+            }
         }
-       else
-           st.push(ast[i]);
-        
-        
+        int[] ans = new int[st.size()];
+        int idx = st.size()-1;
+        while (st.size() > 0) {
+            ans[idx] = st.pop();
+            idx--;
+        }
+        return ans;
     }
-    
-    n=st.size();
-    int arr[]=new int[n];
-    
-    for(int i=n-1;i>=0;i--)
-    arr[i]=st.pop();
-    return arr;
-	
-	
-	}
 }
+
+// class Solution {
+// public int[] asteroidCollision(int[] ast)
+// {
+
+//     Stack<Integer> st = new Stack<>();
+//     int n=ast.length;
+//    for(int i=0;i<n;i++)
+//     {
+//         if(ast[i]<=0)
+//         {
+//             while(!st.isEmpty() && st.peek ()> 0 && st.peek()<ast[i]*-1)
+//                 st.pop();
+            
+//             if(!st.isEmpty() && st.peek()==ast[i]*-1)
+//                 st.pop();
+            
+//             else if(!st.isEmpty() && st.peek() >= -1*ast[i])
+//                 continue;
+//             else
+//                 st.push (ast[i]);
+//         }
+//        else
+//            st.push(ast[i]);
+        
+        
+//     }
+    
+//     n=st.size();
+//     int arr[]=new int[n];
+    
+//     for(int i=n-1;i>=0;i--)
+//     arr[i]=st.pop();
+//     return arr;
+	
+	
+// 	}
+// }
