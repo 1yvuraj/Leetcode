319405eba28c3cc5b0f2ae4daf26819353bbc5e0
YUVRAJ AGGARWAL <87929724+1yvuraj@users.noreply.github.com>
2022-04-15 10:14:16 +0530

    Time: 2 ms (97.27%), Space: 42.7 MB (85.82%) - LeetHub

--- 39-combination-sum/39-combination-sum.java
+++ 39-combination-sum/39-combination-sum.java
@@ -1,35 +1,27 @@
 class Solution {
-   
-    
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
-         List<List<Integer>>ans=new ArrayList<>();
-         ArrayList<Integer>a=new ArrayList<>();
-         combinationSums(candidates,0,target,a,ans);
-         
-         return ans;
+        List<List<Integer>>ans=new ArrayList<>();
+        ArrayList<Integer>a=new ArrayList<>();
+        coinChangeCombination_IN(candidates,target,0,ans,a);
+        return ans;
     }
-    public void combinationSums(int[] candidates, int idx,int amt,  ArrayList<Integer>a, List<List<Integer>>ans) {
-        if(amt<0)
-        {
-            return;
+    public  int coinChangeCombination_IN(int[] coins, int tar, int idx, List<List<Integer>>ans,ArrayList<Integer>a) {
+        if (tar == 0) {
+            List<Integer> base = new ArrayList<>(a);
+            ans.add(base);
+            return 1;
         }
-        if(idx==candidates.length)
-        {
-            if(amt==0)
-            {
-                
-                ans.add(new ArrayList<>(a));
-                return;
+
+        int count = 0;
+        for (int i = idx; i < coins.length; i++) {
+            if (tar - coins[i] >= 0) {
+                a.add(coins[i]);
+                count += coinChangeCombination_IN(coins, tar - coins[i], i, ans,a);
+                 a.remove(a.size()-1);
             }
         }
-        if(idx<candidates.length){
-        a.add(candidates[idx]);
-        combinationSums(candidates,idx,amt-candidates[idx],a,ans);
-        a.remove(a.size() - 1);
-        combinationSums(candidates,idx+1,amt,a,ans);
-        //a.remove(a.size() - 1);
-        }
-       
-        
+    
+        return count;
     }
+
 }
