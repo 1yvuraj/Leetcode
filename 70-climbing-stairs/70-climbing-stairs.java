fd6536bc1c94d2644f655c085060609bcd6515eb
YUVRAJ AGGARWAL <87929724+1yvuraj@users.noreply.github.com>
2022-04-27 21:52:20 +0530

    Time: 0 ms (100.00%), Space: 41.2 MB (23.34%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.java
+++ 70-climbing-stairs/70-climbing-stairs.java
@@ -1,38 +1,12 @@
-// class Solution {
-//     public int climbStairs(int n) {
-//         int []dp=new int[n+1];
-//         return help(n,dp);
-//     }
-//     public int help(int n,int[]dp) {
-//         for(int i=dp.length-1;i>=0;i--){
-       
-//         if(n==0){
-//             dp[i]=1;
-//             continue;
-//         }
-//         dp[i]+=dp[i-1];
-//         if(i-2>=0)
-//         {
-//           dp[i]+=dp[i-2];
-//         }
-//         return dp[];
-//         }
-//     }
-// }
 class Solution {
     public int climbStairs(int n) {
         int[]dp=new int[n+1];
-        return help(n,dp);
+        return solver(n,dp);
     }
-    public int help(int n,int[]dp) {
-        if(n<0)return 0;
-        if(n==0)return 1;
-        if(dp[n]!=0)
-        {
-            return dp[n];
-        }
-        int f1=help(n-1,dp);
-        int f2=help(n-2,dp);
-        return dp[n]=f1+f2;
+    public int solver(int n,int[]dp) {
+     if(n<0)return 0;
+     if(n<=0)return 1;
+     if(dp[n]!=0)return dp[n];
+     return dp[n]=solver(n-1,dp)+solver(n-2,dp);
     }
 }
