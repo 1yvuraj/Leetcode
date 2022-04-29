3937972d13686ad36d0eba97950c13ddddba5bbd
YUVRAJ AGGARWAL <87929724+1yvuraj@users.noreply.github.com>
2022-04-29 21:53:03 +0530

    Time: 0 ms (100.00%), Space: 41.3 MB (24.21%) - LeetHub

---
+++ 62-unique-paths/62-unique-paths.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int uniquePaths(int m, int n) {
+      int[][] dir = { { 0, 1 }, { 1, 0 } };
+        String[] dirS = { "h", "v", "d" };
+        int[][]dp=new int[m+1][n+1];
+       return mazePath2(0, 0, m-1, n-1, dir, dirS,dp);
+    }
+    public static int mazePath2(int sr, int sc, int er, int ec, int[][] dir, String[] dirS,int[][]dp) {
+        if (sr == er && sc == ec) {
+            //System.out.println(psf);
+            return 1;
+        }
+        if(dp[sr][sc]!=0)return dp[sr][sc];
+        int count = 0;
+        for (int d = 0; d < dir.length; d++) {
+            int r = sr + dir[d][0];
+            int c = sc + dir[d][1];
+
+            if (r >= 0 && c >= 0 && r <= er && c <= ec) {
+                count += mazePath2(r, c, er, ec, dir, dirS,dp);
+            }
+
+        }
+        return dp[sr][sc]=count;
+}
+}
