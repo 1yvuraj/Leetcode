d6871ae34d48ab3acc745c423ead76337375c6ef
YUVRAJ AGGARWAL <87929724+1yvuraj@users.noreply.github.com>
2022-05-01 00:28:39 +0530

    Time: 18 ms (42.86%), Space: 75.5 MB (71.43%) - LeetHub

--- 2256-minimum-average-difference/2256-minimum-average-difference.java
+++ 2256-minimum-average-difference/2256-minimum-average-difference.java
@@ -1,60 +1,25 @@
 class Solution {
-//     public int minimumAverageDifference(int[] nums) {
-//         int sum = 0;
-//         long min=(long)1e9;
-//         int left=0;
-        
-        
-//         for (int i = 0; i < nums.length; i++) {
-//             sum += nums[i];
-            
-//         }
-//         int y = 0;
-//         int n=nums.length;
-        
-//         for (int i = 0; i < nums.length; i++) {
-//             left+=nums[i];
-//             int ans=sum-left;
-//             long main = Math.abs(left / (i + 1) - (n - i == 1 ? 0 : ans / (n -i - 1)));
-//             if(min>main)
-//             {
-//                 min=main;
-//                 y=i;
-//             }
-//         }
-        
-//         return y;
-//     }
     public int minimumAverageDifference(int[] nums) {
-	int len = nums.length, res = 0;
-	long min = Integer.MAX_VALUE, sum = 0, leftSum = 0, rightSum = 0;
-	for (int num : nums)
-		sum += num;
-	for (int i = 0; i < len; i++) {
-		leftSum += nums[i];
-		rightSum = sum - leftSum;
-		long diff = Math.abs(leftSum / (i + 1) - (len - i == 1 ? 0 : rightSum / (len -i - 1)));
-		if (diff < min) {
-			min = diff;
-			res = i;
-		}
-	}
-	return res;
+        long sum = 0;
+        long min=Integer.MAX_VALUE;
+        long left=0;
+        long right=0;
+        for (int i = 0; i < nums.length; i++) {
+            sum += nums[i];
+            
+        }
+        int y = 0;
+        int n=nums.length;
+        for (int i = 0; i < nums.length; i++) {
+            left+=nums[i];
+            right=sum-left;
+            long main = Math.abs(left / (i + 1) - (n - i == 1 ? 0 : right / (n -i - 1)));
+            if(min>main)
+            {
+                min=main;
+                y=i;
+            }
+        }
+        return y;
+    }
 }
-}
-// public int minimumAverageDifference(int[] nums) {
-// 	int len = nums.length, res = 0;
-// 	long min = Integer.MAX_VALUE, sum = 0, leftSum = 0, rightSum = 0;
-// 	for (int num : nums)
-// 		sum += num;
-// 	for (int i = 0; i < len; i++) {
-// 		leftSum += nums[i];
-// 		rightSum = sum - leftSum;
-// 		long diff = Math.abs(leftSum / (i + 1) - (len - i == 1 ? 0 : rightSum / (len -i - 1)));
-// 		if (diff < min) {
-// 			min = diff;
-// 			res = i;
-// 		}
-// 	}
-// 	return res;
-// }
