01922db588986bf871f424aad0b4772b7307da95
YUVRAJ AGGARWAL <87929724+1yvuraj@users.noreply.github.com>
2022-05-04 22:40:39 +0530

    Time: 1981 ms (5.03%), Space: 46.4 MB (43.89%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.java
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.java
@@ -1,37 +1,20 @@
 class Solution {
-    
-     
     public int longestCommonSubsequence(String text1, String text2) {
-        int n = text1.length();
-          int m  = text2.length();
-        int[][] dp = new int[n + 1][m + 1];
-          for (int[] d : dp)
-         Arrays.fill(d, -1);
-        
-        return longestCommonSubsequence_dp(text1 , text2 , n , m ,  dp );
-        
+        int[][]dp=new int[text1.length()+1][text2.length()+1];
+        return longestPlaindromicSubsequence_memo(text1,text2,text1.length(),text2.length(),dp);
     }
-public int longestCommonSubsequence_dp(String text1, String text2 , int n , int m ,   int[][] dp) {
-        if (n == 0 || m == 0) {
-            return 0;
+     public static int longestPlaindromicSubsequence_memo(String s1,String s2, int n, int m, int[][] dp) {
+        if (n==0||m==0) {
+            return dp[n][m] = 0;
         }
 
-        if (dp[n][m] != -1)
-            return dp[n][m];
-   
-        if (text1.charAt(n - 1) == text2.charAt(m - 1))
-            dp[n][m]= 1 + longestCommonSubsequence_dp(text1, text2, n - 1, m - 1, dp);
-        else
-            dp[n][m]= Math.max(longestCommonSubsequence_dp(text1, text2, n, m - 1, dp),
-                    longestCommonSubsequence_dp(text1, text2, n - 1, m, dp));
+        if (dp[n][m] != 0) return dp[n][m];
 
-        return dp[n][m];
+        int a = longestPlaindromicSubsequence_memo(s1,s2 ,n - 1, m - 1, dp);
+        int b = longestPlaindromicSubsequence_memo(s1,s2, n - 1,m, dp);
+        int c = longestPlaindromicSubsequence_memo(s1,s2, n, m - 1, dp);
 
+        if (s1.charAt(n-1) == s2.charAt(m-1)) return dp[n][m] = a + 1; 
+         else return dp[n][m] = Math.max(b, c);
+    }
 }
-}
-
-
-
-
-
-
