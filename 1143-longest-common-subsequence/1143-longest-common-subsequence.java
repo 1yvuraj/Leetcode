6c9426b177853d9dea7f6ec51dd8f99423785434
YUVRAJ AGGARWAL <87929724+1yvuraj@users.noreply.github.com>
2022-05-04 22:41:28 +0530

    Time: 63 ms (6.33%), Space: 46.4 MB (43.89%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.java
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.java
@@ -1,6 +1,8 @@
 class Solution {
     public int longestCommonSubsequence(String text1, String text2) {
         int[][]dp=new int[text1.length()+1][text2.length()+1];
+        for (int[] d : dp)
+            Arrays.fill(d, -1);
         return longestPlaindromicSubsequence_memo(text1,text2,text1.length(),text2.length(),dp);
     }
      public static int longestPlaindromicSubsequence_memo(String s1,String s2, int n, int m, int[][] dp) {
@@ -8,7 +10,7 @@ class Solution {
             return dp[n][m] = 0;
         }
 
-        if (dp[n][m] != 0) return dp[n][m];
+        if (dp[n][m] != -1) return dp[n][m];
 
         int a = longestPlaindromicSubsequence_memo(s1,s2 ,n - 1, m - 1, dp);
         int b = longestPlaindromicSubsequence_memo(s1,s2, n - 1,m, dp);
