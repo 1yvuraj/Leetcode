5dd189678bf44144062bf0f9e7a15836667bf232
YUVRAJ AGGARWAL <87929724+1yvuraj@users.noreply.github.com>
2022-05-07 12:33:59 +0530

    Time: 6 ms (83.99%), Space: 41.6 MB (95.86%) - LeetHub

--- 1035-uncrossed-lines/1035-uncrossed-lines.java
+++ 1035-uncrossed-lines/1035-uncrossed-lines.java
@@ -4,30 +4,19 @@ class Solution {
 	   int m=nums2.length;
 	   int dp[][]=new int[n+1][m+1];
 	   for(int[] d:dp) Arrays.fill(d,-1);
-	   return help(nums1,nums2,n,m,dp);
+	   return longestPlaindromicSubsequence_memo(nums1,nums2,n,m,dp);
     }
-    public int help(int[] n1,int[] n2,int n,int m,int[][] dp){
-	    if(n==0)return dp[n][m]=0;
-        else if(m==0)return dp[n][m]=0;
-	    
-	    if(dp[n][m]!=-1){
-	        return dp[n][m];
-	    }
-	    
-	    int a=help(n1,n2,n-1,m-1,dp);
-	    int b=help(n1,n2,n,m-1,dp);
-	     int c=help(n1,n2,n-1,m,dp);
-	    
-	    if(n1[n-1]==n2[m-1]){
-	       return dp[n][m]=a+1;
-	    }
-	    
-	    else{
-	        return dp[n][m]=Math.max(b,c);
-	    }
-	    
-	    
-	    
-	    
-	}
+    public static int longestPlaindromicSubsequence_memo(int[]n1, int[]n2,int N, int M, int[][] dp) {
+         for (int n = 0; n <= N; n++) {
+            for (int m = 0; m <= M; m++) {
+                if (n == 0 || m == 0) {
+                    dp[n][m] = 0;
+                    continue;
+                }
+
+                if (n1[n - 1] == n2[m - 1])  dp[n][m] = dp[n - 1][m - 1]+1; else  dp[n][m] = Math.max(dp[n - 1][m], dp[n][m - 1]);
+            }
+        }
+        return dp[N][M];
+      }
 }
