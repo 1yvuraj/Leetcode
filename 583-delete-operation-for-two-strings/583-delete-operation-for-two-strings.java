a8e82f47c763b836fe0ec34771f9fc1511cecc6b
YUVRAJ AGGARWAL <87929724+1yvuraj@users.noreply.github.com>
2022-05-07 12:15:09 +0530

    Time: 27 ms (7.65%), Space: 43.2 MB (76.06%) - LeetHub

--- 583-delete-operation-for-two-strings/583-delete-operation-for-two-strings.java
+++ 583-delete-operation-for-two-strings/583-delete-operation-for-two-strings.java
@@ -6,21 +6,17 @@ class Solution {
         return minDistance_memo(s, t, s.length(), t.length(), dp);
     }
 
-    public static int minDistance_memo(String s1, String s2, int n, int m, int[][] dp)     {
-         if (n == 0 || m == 0) {
-            return dp[n][m] = n != 0 ? n : m;
+    public static int minDistance_memo(String s1, String s2, int N, int M, int[][] dp) {
+        for (int n = 0; n <= N; n++) {
+            for (int m = 0; m <= M; m++) {
+                if (n == 0 || m == 0) {
+                    dp[n][m] = n != 0 ? n : m;
+                    continue;
+                }
+
+                if (s1.charAt(n - 1) == s2.charAt(m - 1))  dp[n][m] = dp[n - 1][m - 1]; else  dp[n][m] = Math.min(dp[n - 1][m], dp[n][m - 1]) + 1;
+            }
         }
-
-        if (dp[n][m] != -1)
-            return dp[n][m];
-
-        int a = minDistance_memo(s1, s2, n - 1, m - 1, dp);
-        int b = minDistance_memo(s1, s2, n - 1, m, dp);
-        int c = minDistance_memo(s1, s2, n, m - 1, dp);
-
-        if (s1.charAt(n -1) == s2.charAt(m - 1))
-            return dp[n][m] = a;
-        else
-            return dp[n][m] = Math.min(b, c)+1; 
+        return dp[N][M];
     }
 }
