a40334eaca86e4246ea1f9236afe8f794edd2994
YUVRAJ AGGARWAL <87929724+1yvuraj@users.noreply.github.com>
2022-05-07 17:25:26 +0530

    Time: 1 ms (99.66%), Space: 45 MB (84.70%) - LeetHub

--- 64-minimum-path-sum/64-minimum-path-sum.java
+++ 64-minimum-path-sum/64-minimum-path-sum.java
@@ -1,16 +1,28 @@
 class Solution {
     public int minPathSum(int[][] grid) {
-        int[][]dp=new int[grid.length][grid[0].length];
-        return help(grid,0,0,dp);
+        int m=grid.length;
+        int n=grid[0].length;
+        int [][]dir = { { 1,0 }, { 0,1} };
+        int [][]dp = new int[m][n];
+        return mazePath2(0, 0, m - 1, n - 1, dir, grid, dp);
     }
-    public static int help(int[][]arr,int i,int j,int[][]dp) {
-    if(i>=arr.length || j>=arr[0].length)return Integer.MAX_VALUE-1;
-    if(i==arr.length-1 && j==arr[0].length-1)return arr[i][j];
-    if(dp[i][j]!=0)return dp[i][j];
-    int f1=help(arr,i,j+1,dp);
-    int f2=help(arr,i+1,j,dp);
-     
-     return dp[i][j]=Math.min(f1,f2)+arr[i][j];
-    
-  }
+    public static int mazePath2( int sr,int sc,int er, int ec,int[][]dir,int[][]grid, int[][] dp)    {
+        if (sr == er && sc == ec) {
+            return grid[sr][sc];
+        }
+        if (dp[sr][sc] != 0) return dp[sr][sc];
+       
+        int min=(int)1e9;
+         for (int d = 0; d < dir.length; d++) {
+            int r = sr + dir[d][0];
+            int c = sc + dir[d][1];
+            if(r >= 0 && c >= 0 && r <= er && c <= ec) {
+                
+                min = Math.min(min,mazePath2(r, c, er, ec, dir, grid, dp));
+                
+            }
+        }
+         
+        return dp[sr][sc] = min+grid[sr][sc];
+    }
 }
