802714c0dfd0b1c4d266d192ce6ab1d12c3b998c
YUVRAJ AGGARWAL <87929724+1yvuraj@users.noreply.github.com>
2022-05-07 12:01:52 +0530

    Time: 39 ms (15.46%), Space: 50.1 MB (53.63%) - LeetHub

--- 115-distinct-subsequences/115-distinct-subsequences.java
+++ 115-distinct-subsequences/115-distinct-subsequences.java
@@ -5,7 +5,9 @@ class Solution {
               Arrays.fill(d, -1);
         return numDistinct_memo(s,t,s.length(),t.length(),dp);
     }
-    public int numDistinct_memo(String s, String t, int n, int m, int[][] dp) {
+    public int numDistinct_memo(String s1, String s2, int n, int m, int[][] dp) {
+        // if(n==0)return dp[n][m]=0;
+        // else if(m==0)return dp[n][m]=1;
         if (m == 0)
             return dp[n][m] = 1;
         else if (n == 0)
@@ -14,11 +16,11 @@ class Solution {
         if (dp[n][m] != -1)
             return dp[n][m];
 
-        int a = numDistinct_memo(s, t, n - 1, m - 1, dp);
-        int b = numDistinct_memo(s, t, n - 1, m, dp);
+        int a = numDistinct_memo(s1, s2, n - 1, m - 1, dp);
+        int b = numDistinct_memo(s1, s2, n - 1, m, dp);
 
-        if (s.charAt(n - 1) == t.charAt(m - 1))
-            return dp[n][m] = a + b;
+        if (s1.charAt(n-1) == s2.charAt(m - 1))
+            return dp[n][m] = a+b;
         else
             return dp[n][m] = b;
     }
