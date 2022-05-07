9db51c56792e90d42f5a9bbcc319dbe12e9ef5e0
YUVRAJ AGGARWAL <87929724+1yvuraj@users.noreply.github.com>
2022-05-07 10:18:23 +0530

    Time: 148 ms (11.72%), Space: 70.5 MB (37.15%) - LeetHub

--- 516-longest-palindromic-subsequence/516-longest-palindromic-subsequence.java
+++ 516-longest-palindromic-subsequence/516-longest-palindromic-subsequence.java
@@ -1,21 +1,26 @@
 class Solution {
-
     public int longestPalindromeSubseq(String s) {
-        int[][]dp=new int[s.length()+1][s.length()+1];
-        return longestPlaindromicSubsequence_memo(s,0,s.length()-1,dp);
+       int i=0;
+	   int j=s.length()-1;
+	   int dp[][]=new int[s.length()+1][s.length()+1];
+	   for(int[] d:dp) Arrays.fill(d,-1);
+	   return longestPlaindromicSubsequence_memo(s,i,j,dp);
     }
-
-    public static int longestPlaindromicSubsequence_memo(String str, int i, int j, int[][] dp) {
-        if (i >= j) {
-            return dp[i][j] = i == j ? 1 : 0;
-        }
-
-        if (dp[i][j] != 0) return dp[i][j];
-
-        int a = longestPlaindromicSubsequence_memo(str, i + 1, j - 1, dp);
-        int b = longestPlaindromicSubsequence_memo(str, i + 1, j, dp);
-        int c = longestPlaindromicSubsequence_memo(str, i, j - 1, dp);
-
-        if (str.charAt(i) == str.charAt(j)) return dp[i][j] = a + 2; else return dp[i][j] = Math.max(b, c);
-    }
+   public static int longestPlaindromicSubsequence_memo(String str, int i, int j, int[][] dp) {
+         if(i>=j)
+         {
+             return dp[i][j]=i==j?1:0;
+         }
+         if(dp[i][j]!=-1)return dp[i][j];
+         int a=longestPlaindromicSubsequence_memo(str,i+1,j-1,dp);
+         int b=longestPlaindromicSubsequence_memo(str,i+1,j,dp);
+         int c=longestPlaindromicSubsequence_memo(str,i,j-1,dp);
+         if(str.charAt(i)==str.charAt(j))
+         {
+             return dp[i][j]=a+2;
+         }else{
+             return dp[i][j]=Math.max(b,c);
+         }
+      }
+  
 }
