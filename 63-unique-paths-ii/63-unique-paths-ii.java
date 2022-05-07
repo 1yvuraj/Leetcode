fbb5ea75b9856aed0a009d65ecca8364c6ed78b3
YUVRAJ AGGARWAL <87929724+1yvuraj@users.noreply.github.com>
2022-05-07 17:05:28 +0530

    Time: 6 ms (10.52%), Space: 40.2 MB (92.95%) - LeetHub

--- 63-unique-paths-ii/63-unique-paths-ii.java
+++ 63-unique-paths-ii/63-unique-paths-ii.java
@@ -1,36 +1,28 @@
 class Solution {
-
-    public int helper(int sr, int sc, int er, int ec, int[][] dir, int[][] visited, int[][] dp) {
-        if (sr == er && sc == ec) {
-            dp[sr][sc] = 1;
-        }
-
-        if (dp[sr][sc] != 0) return dp[sr][sc];
-
-        visited[sr][sc] = 1;
-        int count = 0;
-        for (int d = 0; d < dir.length; d++) {
-            int r = sr + dir[d][0];
-            int c = sc + dir[d][1];
-
-            if (r >= 0 && c >= 0 && r <= er && c <= ec && visited[r][c] == 0) {
-                count += helper(r, c, er, ec, dir, visited, dp);
-                //dp[r][c] = count;
-            }
-        }
-
-        visited[sr][sc] = 0;
-
-        return dp[sr][sc] = count;
-    }
-
     public int uniquePathsWithObstacles(int[][] obstacleGrid) {
         int m = obstacleGrid.length;
         int n = obstacleGrid[0].length;
         if (obstacleGrid[0][0] == 1 || obstacleGrid[m - 1][n - 1] == 1) return 0;
 
         int dir[][] = { { 0, 1 }, { 1, 0 } };
-        int dp[][] = new int[m+1][n+1];
-        return helper(0, 0, m - 1, n - 1, dir, obstacleGrid, dp);
+        int dp[][] = new int[m][n];
+        return mazePath2(0, 0, m - 1, n - 1, dir, obstacleGrid, dp);
+    }
+     public static int mazePath2(int sr, int sc, int er, int ec, int[][] dir,int[][]obstacleGrid, int[][] dp) {
+        if (sr == er && sc == ec) {
+            return 1;
+        }
+        if (dp[sr][sc] != 0) return dp[sr][sc];
+        int count = 0;
+         obstacleGrid[sr][sc]=1;
+        for (int d = 0; d < dir.length; d++) {
+            int r = sr + dir[d][0];
+            int c = sc + dir[d][1];
+            if (r >= 0 && c >= 0 && r <= er && c <= ec && obstacleGrid[r][c]!=1) {
+                count += mazePath2(r, c, er, ec, dir, obstacleGrid, dp);
+            }
+        }
+         obstacleGrid[sr][sc]=0;
+        return dp[sr][sc] = count;
     }
 }
