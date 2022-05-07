84f76425b99e6f40292dcec93dc456ed7d1e3fc3
YUVRAJ AGGARWAL <87929724+1yvuraj@users.noreply.github.com>
2022-05-07 11:00:23 +0530

    Time: 24 ms (29.17%), Space: 46 MB (83.98%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.java
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.java
@@ -6,17 +6,19 @@ class Solution {
         return longestPlaindromicSubsequence_memo(text1,text2,text1.length(),text2.length(),dp);
     }
      public static int longestPlaindromicSubsequence_memo(String s1,String s2, int n, int m, int[][] dp) {
-        if (n==0||m==0) {
-            return dp[n][m] = 0;
-        }
-
-        if (dp[n][m] != -1) return dp[n][m];
-
-        int a = longestPlaindromicSubsequence_memo(s1,s2 ,n - 1, m - 1, dp);
-        int b = longestPlaindromicSubsequence_memo(s1,s2, n - 1,m, dp);
-        int c = longestPlaindromicSubsequence_memo(s1,s2, n, m - 1, dp);
-
-        if (s1.charAt(n-1) == s2.charAt(m-1)) return dp[n][m] = a + 1; 
-         else return dp[n][m] = Math.max(b, c);
+         if(n==0 || m==0)
+         {
+             return dp[n][m]=0;
+         }
+         if(dp[n][m]!=-1)return dp[n][m];
+         int a=longestPlaindromicSubsequence_memo(s1,s2,n-1,m-1,dp);
+         int b=longestPlaindromicSubsequence_memo(s1,s2,n-1,m,dp);
+         int c=longestPlaindromicSubsequence_memo(s1,s2,n,m-1,dp);
+         if(s1.charAt(n-1)==s2.charAt(m-1))
+         {
+             return dp[n][m]=a+1;
+         }else{
+             return dp[n][m]=Math.max(b,c);
+         }
     }
 }
