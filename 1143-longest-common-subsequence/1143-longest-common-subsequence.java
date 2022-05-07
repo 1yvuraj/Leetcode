5220e418b82c84abd9c1d3847c0f347129b095c7
YUVRAJ AGGARWAL <87929724+1yvuraj@users.noreply.github.com>
2022-05-07 11:04:15 +0530

    Time: 18 ms (52.61%), Space: 46.2 MB (62.52%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.java
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.java
@@ -1,24 +1,43 @@
 class Solution {
+
     public int longestCommonSubsequence(String text1, String text2) {
-        int[][]dp=new int[text1.length()+1][text2.length()+1];
-        for (int[] d : dp)
-            Arrays.fill(d, -1);
-        return longestPlaindromicSubsequence_memo(text1,text2,text1.length(),text2.length(),dp);
+        int[][] dp = new int[text1.length() + 1][text2.length() + 1];
+        for (int[] d : dp) Arrays.fill(d, -1);
+        return longestPlaindromicSubsequence_memo(text1, text2, text1.length(), text2.length(), dp);
     }
-     public static int longestPlaindromicSubsequence_memo(String s1,String s2, int n, int m, int[][] dp) {
-         if(n==0 || m==0)
-         {
-             return dp[n][m]=0;
-         }
-         if(dp[n][m]!=-1)return dp[n][m];
-         int a=longestPlaindromicSubsequence_memo(s1,s2,n-1,m-1,dp);
-         int b=longestPlaindromicSubsequence_memo(s1,s2,n-1,m,dp);
-         int c=longestPlaindromicSubsequence_memo(s1,s2,n,m-1,dp);
-         if(s1.charAt(n-1)==s2.charAt(m-1))
-         {
-             return dp[n][m]=a+1;
-         }else{
-             return dp[n][m]=Math.max(b,c);
-         }
+
+    // public static int longestPlaindromicSubsequence_memo(String s1, String s2, int n, int m, int[][] dp) {
+    //     if (n == 0 || m == 0) {
+    //         return dp[n][m] = 0;
+    //     }
+    //     if (dp[n][m] != -1) return dp[n][m];
+    //     int a = longestPlaindromicSubsequence_memo(s1, s2, n - 1, m - 1, dp);
+    //     int b = longestPlaindromicSubsequence_memo(s1, s2, n - 1, m, dp);
+    //     int c = longestPlaindromicSubsequence_memo(s1, s2, n, m - 1, dp);
+    //     if (s1.charAt(n - 1) == s2.charAt(m - 1)) {
+    //         return dp[n][m] = a + 1;
+    //     } else {
+    //         return dp[n][m] = Math.max(b, c);
+    //     }
+    // }
+
+    public static int longestPlaindromicSubsequence_memo(String s1, String s2, int N, int M, int[][] dp) {
+        for (int n = 0; n <= N; n++) {
+            for (int m = 0; m <= M; m++) {
+                if (n == 0 || m == 0) {
+                   dp[n][m] = 0;
+                    continue;
+                }
+                
+               
+                if (s1.charAt(n - 1) == s2.charAt(m - 1)) {
+                     dp[n][m] = dp[n-1][m-1] + 1;
+                } else {
+                    dp[n][m] = Math.max(dp[n-1][m], dp[n][m-1]);
+                }
+            }
+            
+        }
+        return dp[N][M];
     }
 }
