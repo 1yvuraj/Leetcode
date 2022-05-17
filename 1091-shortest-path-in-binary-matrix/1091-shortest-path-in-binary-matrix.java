c0f374aa356c8fb68276e89d48d8159dc6789dd2
YUVRAJ AGGARWAL <87929724+1yvuraj@users.noreply.github.com>
2022-05-17 11:27:24 +0530

    Time: 22 ms (64.63%), Space: 60.8 MB (32.40%) - LeetHub

--- 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.java
+++ 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.java
@@ -1,85 +1,23 @@
 class Solution {
 
-    public class pair {
-        int x = 0;
-        int y = 0;
-        int move = 1;
-
-        pair(int x, int y, int move) {
-            this.x = x;
-            this.y = y;
-            this.move = move;
-        }
-    }
-
     public int shortestPathBinaryMatrix(int[][] grid) {
         int[][] dir = { { -1, 0 }, { -1, 1 }, { 0, 1 }, { 1, 1 }, { 1, 0 }, { 1, -1 }, { 0, -1 }, { -1, -1 } };
+        Queue<int[]> ans = new LinkedList<>();
         if (grid[0][0] == 1 || grid[grid.length - 1][grid[0].length - 1] == 1) return -1;
-        int ans = shortesPath(grid.length - 1, grid[0].length - 1, grid, dir);
-        return ans;
-    }
-
-    // pair<int,string>
-    public int shortesPath(int er, int ec, int[][] mat, int[][] dir) {
-        LinkedList<pair> list = new LinkedList<>();
-        list.addLast(new pair(0, 0, 1));
-        while (list.size() > 0) {
-            pair ans = list.removeFirst();
-            int x = ans.x;
-            int y = ans.y;
-            int move = ans.move;
-
+        ans.add(new int[] { 0, 0, 1 });
+        grid[0][0] = 1;
+        while (ans.size() > 0) {
+            int[] arr = ans.poll();
             for (int d = 0; d < dir.length; d++) {
-                if (x == er && y == ec) return ans.move;
-                int r = x + dir[d][0];
-                int c = y + dir[d][1];
-
-                if (r >= 0 && c >= 0 && r <= er && c <= ec && mat[r][c] == 0) {
-                    list.addLast(new pair(r, c, move + 1));
-                    mat[r][c] = 1;
+                if (arr[0] == grid.length - 1 && arr[1] == grid[0].length - 1) return arr[2];
+                int r = arr[0] + dir[d][0];
+                int c = arr[1] + dir[d][1];
+                if (r >= 0 && c >= 0 && r <= grid.length - 1 && c <= grid[0].length - 1 && grid[r][c] == 0) {
+                    ans.add(new int[] { r, c, arr[2] + 1 });
+                    grid[r][c] = 1;
                 }
             }
         }
         return -1;
     }
 }
-//  class Solution {
-//      class Pair{
-//     int x;
-//     int y;
-//     int count;
-//     Pair(int x, int y, int count){
-//         this.x = x;
-//         this.y = y;
-//         this.count = count;
-//     }
-// }
-// public int shortestPathBinaryMatrix(int[][] grid) {
-//     return BFS(grid, 0, 0, grid.length-1, grid[0].length-1);
-// }
-// public int BFS(int grid[][], int start_x, int start_y , int target_x, int target_y){
-//     Queue<Pair> q = new LinkedList<>();
-//     q.add(new Pair(start_x, start_y, 1));
-//     while(q.size()>0){
-//         Pair rem = q.remove();
-//         int x = rem.x;
-//         int y = rem.y;
-//         int count = rem.count;
-//         if(x>=0 && y>=0 && x<grid.length && y<grid[0].length && grid[x][y]!=1 ){
-//         grid[x][y] = 1;
-//         if(x==target_x && y== target_y)
-//             return rem.count;
-//         q.add(new Pair(x-1, y, count+1 ));
-//         q.add(new Pair(x-1, y+1, count+1));
-//         q.add(new Pair(x, y+1 , count+1));
-//         q.add(new Pair(x+1, y+1, count+1));
-//         q.add(new Pair(x+1, y, count+1));
-//         q.add(new Pair(x+1, y-1, count+1));
-//         q.add(new Pair(x, y-1, count+1));
-//         q.add(new Pair(x-1, y-1, count+1));
-//       }
-//    }
-//     return -1;
-// }
-// }
-// // }
