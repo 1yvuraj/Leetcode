7fb29bf0331efd61c228478f639038f0588a14b6
YUVRAJ AGGARWAL <87929724+1yvuraj@users.noreply.github.com>
2022-05-18 13:08:22 +0530

    Time: 3 ms (87.88%), Space: 43.3 MB (27.44%) - LeetHub

--- 987-vertical-order-traversal-of-a-binary-tree/987-vertical-order-traversal-of-a-binary-tree.java
+++ 987-vertical-order-traversal-of-a-binary-tree/987-vertical-order-traversal-of-a-binary-tree.java
@@ -1,68 +1,3 @@
-// class Solution
-// {
-//     //Function to find the vertical order traversal of Binary Tree.
-//     static class pair implements Comparable<pair>{
-//         TreeNode node;
-//         int h;
-//         int d;
-//         pair(TreeNode node,int h,int d)
-//         {
-//             this.node=node;
-//             this.h=h;
-//             this.d=d;
-//         }
-//         public int compareTo(pair other){
-//             if(this.d == other.d){
-//                 return this.node.val - other.node.val;
-//             } else {
-//                 return this.d - other.d;
-//             }
-//         }
-//     }
-//     public List<List<Integer>> verticalTraversal(TreeNode root) {
-    
-//        LinkedList<pair>list=new LinkedList<>();
-//        HashMap<Integer,PriorityQueue<Integer>>map=new HashMap<>();
-//        int min =(int)1e9;
-//        int max=-(int)1e9;
-//        list.add(new pair(root,0,1));
-//        while(list.size()>0)
-//        {
-//            pair p=list.removeFirst();
-//            if(!map.containsKey(p.h))
-//            {
-//                map.put(p.h, new PriorityQueue<>());
-//                map.get(p.h).add(p.node.val);
-
-//            }else{
-//             map.get(p.h).add(p.node.val);
-//            }
-//            min=Math.min(min,p.h);
-//            max=Math.max(max,p.h);
-//            if(p.node.left!=null)
-//            {
-//                list.addLast(new pair(p.node.left,p.h-1,p.d+1));
-//            }
-//            if(p.node.right!=null)
-//            {
-//                list.addLast(new pair(p.node.right,p.h+1,p.d+1));
-//            }
-//        }
-//        List<List<Integer>>ans=new ArrayList<>();
-//        for(int i=min;i<=max;i++)
-//        {
-//         PriorityQueue<Integer>p=map.get(i);
-//         ArrayList<Integer>a=new ArrayList<>();
-//         while(p.size()>0)
-//         {
-//             a.add(p.remove());
-//         }
-//         ans.add(a);
-//        }
-//        return ans;
-
-//     }
-// }
 class Solution
 {
     //Function to find the vertical order traversal of Binary Tree.
@@ -84,28 +19,26 @@ class Solution
             }
         }
     }
-    
     public List<List<Integer>> verticalTraversal(TreeNode root) {
     
-       LinkedList<pair>list = new LinkedList<>();
+       LinkedList<pair>list=new LinkedList<>();
        HashMap<Integer,PriorityQueue<pair>>map=new HashMap<>();
-       int min =0;
-       int max=0;
+       int min =(int)1e9;
+       int max=-(int)1e9;
        list.add(new pair(root,0,1));
        while(list.size()>0)
        {
-           pair p = list.removeFirst();
-           if(!map.containsKey(p.h)){
+           pair p=list.removeFirst();
+           if(!map.containsKey(p.h))
+           {
                map.put(p.h, new PriorityQueue<>());
-               map.get(p.h).add( p );
-           }
-           else{
+               map.get(p.h).add(p);
+
+           }else{
             map.get(p.h).add(p);
            }
-           
            min=Math.min(min,p.h);
            max=Math.max(max,p.h);
-           
            if(p.node.left!=null)
            {
                list.addLast(new pair(p.node.left,p.h-1,p.d+1));
@@ -115,7 +48,6 @@ class Solution
                list.addLast(new pair(p.node.right,p.h+1,p.d+1));
            }
        }
-        
        List<List<Integer>>ans=new ArrayList<>();
        for(int i=min;i<=max;i++)
        {
@@ -123,7 +55,7 @@ class Solution
         ArrayList<Integer>a=new ArrayList<>();
         while(p.size()>0)
         {
-            a.add(p.remove().node.val   );
+            a.add(p.remove().node.val);
         }
         ans.add(a);
        }
