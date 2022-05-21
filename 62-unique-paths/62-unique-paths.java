28e63df2b505f08965209d34fbb850fb8d1564af
YUVRAJ AGGARWAL <87929724+1yvuraj@users.noreply.github.com>
2022-05-21 22:14:30 +0530

    Time: 2 ms (7.65%), Space: 40.9 MB (53.04%) - LeetHub

--- 62-unique-paths/62-unique-paths.java
+++ 62-unique-paths/62-unique-paths.java
@@ -2,24 +2,42 @@ class Solution {
 
     public int uniquePaths(int m, int n) {
         int[][] dir = { { 1, 0 }, { 0, 1 } };
-        String[] dirS = { "V", "H" };
-        int[][] dp = new int[m][n];
-        return mazePath2(0, 0, m - 1, n - 1, dir, dirS, dp);
+        int[][]dp=new int[m][n];
+        return help(0, 0, m - 1, n - 1, dir,dp);
     }
 
-    public static int mazePath2(int sr, int sc, int er, int ec, int[][] dir, String[] dirS, int[][] dp) {
-        if (sr == er && sc == ec) {
-            return 1;
-        }
-        if (dp[sr][sc] != 0) return dp[sr][sc];
-        int count = 0;
-        for (int d = 0; d < dir.length; d++) {
-            int r = sr + dir[d][0];
-            int c = sc + dir[d][1];
-            if (r >= 0 && c >= 0 && r <= er && c <= ec) {
-                count += mazePath2(r, c, er, ec, dir, dirS, dp);
+    // public int help(int sr, int sc, int er, int ec, int[][] dir) {
+    //     if (sr == er && sc == ec) return 1;
+    //     int count = 0;
+    //     for (int d = 0; d < dir.length; d++) {
+    //         int r = sr + dir[d][0];
+    //         int c = sc + dir[d][1];
+    //         if (r >= 0 && c >= 0 && r <= er && c <= ec) {
+    //             count += help(r, c, er, ec, dir);
+    //         }
+    //     }
+    //     return count;
+    // }
+
+    public int help(int sr, int sc, int er, int ec, int[][] dir,int[][]dp) {
+        for (int i = er; i >= 0; i--) {
+            for (int j = ec; j >= 0; j--) {
+                if (i == er && j == ec) 
+                {
+                    dp[i][j]= 1;
+                    continue;
+                }
+                int count = 0;
+                for (int d = 0; d < dir.length; d++) {
+                    int r = i + dir[d][0];
+                    int c = j + dir[d][1];
+                    if (r >= 0 && c >= 0 && r <= er && c <= ec) {
+                        count += dp[r][c];
+                    }
+                }
+                dp[i][j]=count;
             }
         }
-        return dp[sr][sc] = count;
+        return dp[0][0];
     }
 }
