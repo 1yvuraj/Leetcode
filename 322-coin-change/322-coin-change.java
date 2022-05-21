b06203a8e5bb4cd12b18bd76df2a6ec0110e6e5e
YUVRAJ AGGARWAL <87929724+1yvuraj@users.noreply.github.com>
2022-05-21 22:55:38 +0530

    Time: 59 ms (17.61%), Space: 44.8 MB (61.57%) - LeetHub

--- 322-coin-change/322-coin-change.java
+++ 322-coin-change/322-coin-change.java
@@ -1,27 +1,23 @@
 class Solution {
     public int coinChange(int[] coins, int amount) {
-        int dp[][]=new int[amount+1][coins.length+1];
-        //Arrays.fill(dp,-1);
-        int ans=coinChange(coins,amount,0,dp);
+        int dp[]=new int[amount+1];
+        Arrays.fill(dp,-1);
+        int ans=coinChange(coins,amount,dp);
         return ans==(int)1e9?-1:ans;
     }
     
-    public int coinChange(int[] coins, int tar,int idx,int dp[][]){
+    public int coinChange(int[] coins, int tar,int dp[]){
+        if(tar<0)return (int)1e9;
         if(tar==0){
-            return dp[tar][idx]=0;
+            return dp[tar]=0;
         }
-        if(dp[tar][idx]!=0){
-            return dp[tar][idx];
+        if(dp[tar]!=-1){
+            return dp[tar];
         }
         int minCoins=(int)1e9;
-        for(int i=idx;i<coins.length;i++){
-            if(tar-coins[i]>=0){
-                int recMin=coinChange(coins,tar-coins[i],i,dp);
-                if(recMin!=(int)1e9 && recMin+1<minCoins){
-                    minCoins=recMin+1;
-                }
-            }
+        for(int i=0;i<coins.length;i++){
+            minCoins=Math.min(minCoins,coinChange(coins,tar-coins[i],dp)+1);
         }
-        return dp[tar][idx]= minCoins;
+        return dp[tar]= minCoins;
     }
 }
