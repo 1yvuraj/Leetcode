8a17b08b69bdce714fc93afb5b74120bf2e664bd
YUVRAJ AGGARWAL <87929724+1yvuraj@users.noreply.github.com>
2022-05-21 23:41:37 +0530

    Time: 1871 ms (5.00%), Space: 89.6 MB (6.62%) - LeetHub

--- 322-coin-change/322-coin-change.java
+++ 322-coin-change/322-coin-change.java
@@ -3,39 +3,42 @@ class Solution {
         int dp[]=new int[amount+1];
         //Arrays.fill(dp,-1);
         int ans=coinChange(coins,amount,dp);
-        
+        for(int i=0;i<dp.length;i++)
+        {
+            System.out.println(dp[i]);
+        }
         return ans==(int)1e9?-1:ans;
     }
     
-    public int coinChange(int[] coins, int tar,int dp[]){
-        if(tar<0)return (int)1e9;
-        if(tar==0){
-            return dp[tar]=0;
-        }
-        if(dp[tar]!=0){
-            return dp[tar];
-        }
-        int minCoins=(int)1e9;
-        for(int i=0;i<coins.length;i++){
-            minCoins=Math.min(minCoins,coinChange(coins,tar-coins[i],dp)+1);
-        }
-        return dp[tar]=minCoins;
-    }
     // public int coinChange(int[] coins, int tar,int dp[]){
-    //     for(int i=0;i<dp.length;i++)
-    //     {
-    //     if(i==0){
-    //         dp[i]=0;
-    //         continue;
+    //     if(tar<0)return (int)1e9;
+    //     if(tar==0){
+    //         return dp[tar]=0;
+    //     }
+    //     if(dp[tar]!=0){
+    //         return dp[tar];
     //     }
     //     int minCoins=(int)1e9;
-    //     for(int k=0;k<coins.length;k++){
-    //         if(tar-coins[k]>=0){
-    //         minCoins=Math.min(minCoins,dp[tar-coins[k]]+1);
-    //         }
+    //     for(int i=0;i<coins.length;i++){
+    //         minCoins=Math.min(minCoins,coinChange(coins,tar-coins[i],dp)+1);
     //     }
-    //     dp[i]= minCoins;
-    //     }  
-    //     return dp[dp.length-1];
+    //     return dp[tar]=minCoins;
     // }
+    public int coinChange(int[] coins, int tar,int dp[]){
+        for(int i=0;i<dp.length;i++)
+        {
+        if(i==0){
+            dp[i]=0;
+            continue;
+        }
+        int minCoins=(int)1e9;
+        for(int k=0;k<coins.length;k++){
+            if(i-coins[k]>=0){
+            minCoins=Math.min(minCoins,dp[i-coins[k]]+1);
+            }
+        }
+        dp[i]= minCoins;
+        }  
+        return dp[dp.length-1];
+    }
 }
