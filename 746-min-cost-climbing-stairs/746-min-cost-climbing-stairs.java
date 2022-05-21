d8f3783c8eb0265bce847132898e989e20c6bab5
YUVRAJ AGGARWAL <87929724+1yvuraj@users.noreply.github.com>
2022-05-21 18:28:29 +0530

    Time: 3 ms (14.35%), Space: 43.5 MB (58.59%) - LeetHub

--- 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.java
+++ 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.java
@@ -4,22 +4,13 @@ class Solution {
         return solve(cost,dp,cost.length);
     }
     public int solve(int[] cost,int []dp,int n) {
-        
-        if(n<=1)return dp[n]=cost[n];
+        if(n==0)return dp[n]=cost[n];
+        if(n<0)return 0;
         if(dp[n]!=0)return dp[n];
         int f1=solve(cost,dp,n-1);
         int f2=solve(cost,dp,n-2);
         return dp[n]=Math.min(f1,f2)+(n==cost.length?0:cost[n]);
         
     }
-//     public int solve(int[] cost,int[]dp) {
-//         dp[0]=cost[0];
-//          dp[1]=Math.min(cost[0],cost[1]);
-//         for(int i=2;i<cost.length;i++)
-//         {
-//             dp[i]=Math.min(dp[i-2],dp[i-1])+cost[i];
-//         }
-//         return dp[dp.length-1];
-        
-//     }
+
 }
