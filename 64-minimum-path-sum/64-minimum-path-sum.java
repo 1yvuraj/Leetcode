7361a0a3bcd4314459b0c716a8be1d3ef680fe1e
YUVRAJ AGGARWAL <87929724+1yvuraj@users.noreply.github.com>
2022-05-22 22:43:39 +0530

    Time: 6 ms (10.08%), Space: 45.9 MB (51.01%) - LeetHub

--- 64-minimum-path-sum/64-minimum-path-sum.java
+++ 64-minimum-path-sum/64-minimum-path-sum.java
@@ -1,24 +1,45 @@
 class Solution {
+
     public int minPathSum(int[][] grid) {
-        int m=grid.length;
-        int n=grid[0].length;
-        int [][]dir = { { 1,0 }, { 0,1} };
-        int [][]dp = new int[m][n];
-        return help(grid,0, 0, m - 1, n - 1, dir,dp);
+        int m = grid.length;
+        int n = grid[0].length;
+        int[][] dir = { { 1, 0 }, { 0, 1 } };
+        int[][] dp = new int[m][n];
+        return help(grid, 0, 0, m - 1, n - 1, dir, dp);
     }
-    public int help(int[][]grid,int sr, int sc, int er, int ec, int[][] dir,int[][]dp) {
-        
-        if (sr == er && sc == ec) return grid[sr][sc];
-        if(dp[sr][sc]!=0)return dp[sr][sc];
-        int count = (int)1e9;
-        for (int d = 0; d < dir.length; d++) {
-            int r = sr + dir[d][0];
-            int c = sc + dir[d][1];
-            if (r >= 0 && c >= 0 && r <= er && c <= ec) {
-               count=Math.min(count, help(grid,r, c, er, ec, dir,dp)+grid[sr][sc]);
+
+    // public int help(int[][] grid, int sr, int sc, int er, int ec, int[][] dir, int[][] dp) {
+    //     if (sr == er && sc == ec) return grid[sr][sc];
+    //     if (dp[sr][sc] != 0) return dp[sr][sc];
+    //     int count = (int) 1e9;
+    //     for (int d = 0; d < dir.length; d++) {
+    //         int r = sr + dir[d][0];
+    //         int c = sc + dir[d][1];
+    //         if (r >= 0 && c >= 0 && r <= er && c <= ec) {
+    //             count = Math.min(count, help(grid, r, c, er, ec, dir, dp) + grid[sr][sc]);
+    //         }
+    //     }
+    //     return dp[sr][sc] = count;
+    // }
+
+    public int help(int[][] grid, int sr, int sc, int er, int ec, int[][] dir, int[][] dp) {
+        for (int i = er; i >= 0; i--) {
+            for (int j = ec; j >= 0; j--) {
+                if (i == er && j == ec) {
+                    dp[i][j] = grid[i][j];
+                    continue;
+                }
+                int count = (int)1e9;
+                for (int d = 0; d < dir.length; d++) {
+                    int r = i + dir[d][0];
+                    int c = j + dir[d][1];
+                    if (r >= 0 && c >= 0 && r <= er && c <= ec) {
+                        count =Math.min(count,dp[r][c]);
+                    }
+                }
+                dp[i][j] = count+grid[i][j];
             }
         }
-        return dp[sr][sc]=count;
+        return dp[0][0];
     }
-    
 }
