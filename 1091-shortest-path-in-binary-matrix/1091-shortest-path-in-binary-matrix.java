da323eb9bf32927ccf68a3e523c77dac0b7d7aa5
YUVRAJ AGGARWAL <87929724+1yvuraj@users.noreply.github.com>
2022-05-22 23:07:00 +0530

    Time: 24 ms (59.21%), Space: 60.7 MB (32.06%) - LeetHub

--- 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.java
+++ 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.java
@@ -2,18 +2,22 @@ class Solution {
 
     public int shortestPathBinaryMatrix(int[][] grid) {
         int[][] dir = { { -1, 0 }, { -1, 1 }, { 0, 1 }, { 1, 1 }, { 1, 0 }, { 1, -1 }, { 0, -1 }, { -1, -1 } };
-        Queue<int[]> ans = new LinkedList<>();
+        LinkedList<int[]> list = new LinkedList<>();
+        list.addLast(new int[] { 0, 0, 1 });
         if (grid[0][0] == 1 || grid[grid.length - 1][grid[0].length - 1] == 1) return -1;
-        ans.add(new int[] { 0, 0, 1 });
-        grid[0][0] = 1;
-        while (ans.size() > 0) {
-            int[] arr = ans.poll();
+        while (list.size() > 0) {
+            int[] arr = list.removeFirst();
+            int x = arr[0];
+            int y = arr[1];
+            int move = arr[2];
+            grid[x][y] = 1;
             for (int d = 0; d < dir.length; d++) {
-                if (arr[0] == grid.length - 1 && arr[1] == grid[0].length - 1) return arr[2];
-                int r = arr[0] + dir[d][0];
-                int c = arr[1] + dir[d][1];
-                if (r >= 0 && c >= 0 && r <= grid.length - 1 && c <= grid[0].length - 1 && grid[r][c] == 0) {
-                    ans.add(new int[] { r, c, arr[2] + 1 });
+                if (x == grid.length - 1 && y == grid[0].length - 1) return move;
+                int r = x + dir[d][0];
+                int c = y + dir[d][1];
+                
+                if (r >= 0 && c >= 0 && r <= grid.length - 1 && c <= grid[0].length - 1 && grid[r][c]==0) {
+                    list.addLast(new int[] { r, c, move + 1 });
                     grid[r][c] = 1;
                 }
             }
