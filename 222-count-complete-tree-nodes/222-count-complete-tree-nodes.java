caee9f182b6779c5ada9fc99e4a1d005ce142d6f
YUVRAJ AGGARWAL <87929724+1yvuraj@users.noreply.github.com>
2022-05-27 13:24:18 +0530

    Time: 0 ms (100.00%), Space: 44.5 MB (99.53%) - LeetHub

--- 222-count-complete-tree-nodes/222-count-complete-tree-nodes.java
+++ 222-count-complete-tree-nodes/222-count-complete-tree-nodes.java
@@ -1,24 +1,37 @@
+// class Solution {
+//     public int countNodes(TreeNode root) {
+//         if(root==null)return 0;
+//         TreeNode ln=root.left;
+//         int lnc=0;
+//         while(ln!=null)
+//         {
+//             ln=ln.left;
+//             lnc++;
 
+//         }
+//          TreeNode rn=root.right;
+//         int rnc=0;
+//         while(rn!=null)
+//         {
+//             rn=rn.right;
+//             rnc++;
+
+//         }
+//         if(rnc==lnc)return (1<<(lnc+1))-1;
+//         else return countNodes(root.left)+countNodes(root.right)+1;
+//     }
+// }
 class Solution {
+    int ans = 0;
+
     public int countNodes(TreeNode root) {
-        if(root==null)return 0;
-        TreeNode ln=root.left;
-        int lnc=0;
-        while(ln!=null)
-        {
-            ln=ln.left;
-            lnc++;
-            
-        }
-         TreeNode rn=root.right;
-        int rnc=0;
-        while(rn!=null)
-        {
-            rn=rn.right;
-            rnc++;
-            
-        }
-        if(rnc==lnc)return (1<<(lnc+1))-1;
-        else return countNodes(root.left)+countNodes(root.right)+1;
+        help(root);
+        return ans;
+    }
+    public void help(TreeNode root) {
+        if (root == null) return;
+        ans++;
+        help(root.left);
+        help(root.right);
     }
 }
