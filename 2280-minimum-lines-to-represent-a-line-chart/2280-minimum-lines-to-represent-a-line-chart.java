020c6553d981010bbc1d9799e7886f777fb20407
YUVRAJ AGGARWAL <87929724+1yvuraj@users.noreply.github.com>
2022-05-28 11:34:52 +0530

    Time: 87 ms (19.31%), Space: 107.3 MB (32.75%) - LeetHub

--- 2280-minimum-lines-to-represent-a-line-chart/2280-minimum-lines-to-represent-a-line-chart.java
+++ 2280-minimum-lines-to-represent-a-line-chart/2280-minimum-lines-to-represent-a-line-chart.java
@@ -1,30 +1,17 @@
+
 class Solution {
-    public int minimumLines(int[][] c) {
+    public int minimumLines(int[][] stock) {
+        int n = stock.length,res = 0;
+        Arrays.sort(stock,(a,b) -> a[0] - b[0]);
         
-        int ans=0;
-        Arrays.sort(c,(a,b) -> a[0] - b[0]);
-        for(int i=0;i<c.length-1;i++){
-            if(i == c.length-2){ans++; continue;} 
-            int first=(c[i+1][1]-c[i][1])*(c[i+2][0]-c[i+1][0]);
-            int second=(c[i+2][1]-c[i+1][1])*(c[i+1][0]-c[i][0]);
-             if(first!=second)ans++;
+        for(int i=0;i<n-1;++i){
+            if(i == n-2){res++; continue;} // it's not part of previous line and there is no next line
+                int x1 = stock[i+1][1] - stock[i][1] , x2 = stock[i+2][1] - stock[i+1][1];
+                int y1 = stock[i+1][0] - stock[i][0] , y2 = stock[i+2][0] - stock[i+1][0];
+				// Cross Multiplication 
+				// x1/y1 != x2/y2 can be written as x1 * y2 != x2 * y1
+                if(x1 * y2 != x2 * y1) res++;
         }
-        return ans;
+        return res;
     }
 }
-// class Solution {
-//     public int minimumLines(int[][] stock) {
-//         int n = stock.length,res = 0;
-//         Arrays.sort(stock,(a,b) -> a[0] - b[0]);
-        
-//         for(int i=0;i<n-1;++i){
-//             if(i == n-2){res++; continue;} // it's not part of previous line and there is no next line
-//                 int x1 = stock[i+1][1] - stock[i][1] , x2 = stock[i+2][1] - stock[i+1][1];
-//                 int y1 = stock[i+1][0] - stock[i][0] , y2 = stock[i+2][0] - stock[i+1][0];
-// 				// Cross Multiplication 
-// 				// x1/y1 != x2/y2 can be written as x1 * y2 != x2 * y1
-//                 if(x1 * y2 != x2 * y1) res++;
-//         }
-//         return res;
-//     }
-// }
