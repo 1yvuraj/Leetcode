6beb27881a51a5f411c821b515f11e78494deb0c
YUVRAJ AGGARWAL <87929724+1yvuraj@users.noreply.github.com>
2022-05-28 12:56:46 +0530

    Time: 1 ms (94.20%), Space: 47.8 MB (42.97%) - LeetHub

--- 124-binary-tree-maximum-path-sum/124-binary-tree-maximum-path-sum.java
+++ 124-binary-tree-maximum-path-sum/124-binary-tree-maximum-path-sum.java
@@ -14,39 +14,23 @@
  * }
  */
 class Solution {
-     public static class pair {
-        int RtNpath = -(int)1e9;
-        int NtNpath = -(int)1e9;
-    }
+    int max=-(int)1e9;
     public int maxPathSum(TreeNode root) {
+        max=-(int)1e9;
+        help(root);
+         return max;
+    }
+    public int help(TreeNode root) {
         if(root==null)return 0;
-        if(root.left==null && root.right==null )return root.val;
+        int l=help(root.left);
+        int r=help(root.right);
+        int ldash=Math.max(0,l);
+        int rdash=Math.max(0,r);
+        int sumNTN=ldash+rdash+root.val;
+        max=Math.max(max,sumNTN);
+        return Math.max(ldash,rdash)+root.val;
         
-        return Math.max(maxPathSu(root).NtNpath,maxPathSu(root).RtNpath);
-    }
-
-    
-    public static int max(int... arr)
-    {
-        int maxsum=arr[0];
-        for(int e:arr)
-        {
-            maxsum=Math.max(e,maxsum);
-        }
-        return maxsum;
-    }
-
-    public static pair maxPathSu(TreeNode root) {
-        if (root == null) return new pair();
-       
-        pair l = maxPathSu(root.left);
-        pair r = maxPathSu(root.right);
-        pair ans = new pair();
-        int RtNpath=Math.max(l.RtNpath,r.RtNpath)+root.val;
-        int NtNpath=max(l.NtNpath,r.NtNpath,l.RtNpath+r.RtNpath+root.val,root.val,RtNpath);
-        ans.RtNpath=Math.max(root.val,RtNpath);
-        ans.NtNpath=NtNpath;
         
-        return ans;
+        
     }
 }
