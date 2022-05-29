54145200e8521112eca76df9b4008f690c8ab59b
YUVRAJ AGGARWAL <87929724+1yvuraj@users.noreply.github.com>
2022-05-29 23:45:27 +0530

    Time: 752 ms (16.67%), Space: 118.8 MB (16.67%) - LeetHub

--- 2288-apply-discount-to-prices/2288-apply-discount-to-prices.java
+++ 2288-apply-discount-to-prices/2288-apply-discount-to-prices.java
@@ -1,42 +1,5 @@
-class Solution {
-    public static boolean isNumeric(String str)
-    {
-        if(str.isEmpty()) return false;
-        
-        for (char c : str.toCharArray())
-        {
-            if (!Character.isDigit(c)) return false;
-        }
-        return true;
-    }
-    public String discountPrices(String sentence, int discount) {
-        
-        String ans="";
-        String[]arr=sentence.split(" ");
-        
-        for(int i=0;i<arr.length;i++)
-        {
-            
-             if(arr[i].charAt(0)=='$' && isNumeric(arr[i].substring(1)))
-            {
-                  double dc=(double)(Double.parseDouble(arr[i].substring(1))*(discount/100.0));
-                  double ap=(Double.parseDouble(arr[i].substring(1))-dc);
-                  if(i==arr.length-1 && arr[arr.length-1].charAt(0)=='$')ans+="$"+String.format("%.2f", ap);
-                 else ans+="$"+String.format("%.2f", ap)+" ";
-                 
-               
-            }else{
-                 if(i==arr.length-1)ans+=arr[i];
-                 else ans+=arr[i]+" ";
-                 
-             }
-           
-        }
-        return ans;
-    }
-}
 // class Solution {
-// public static boolean isNumeric(String str)
+//     public static boolean isNumeric(String str)
 //     {
 //         if(str.isEmpty()) return false;
         
@@ -46,31 +9,68 @@ class Solution {
 //         }
 //         return true;
 //     }
-    
-    
 //     public String discountPrices(String sentence, int discount) {
-//         String[] words = sentence.split(" ");
-//         for(int i=0;i<words.length;i++){
-//             if(words[i].charAt(0)=='$' &&  isNumeric(words[i].substring(1))){
-//                 // System.out.println(words[i]);
-//                 double price = Double.parseDouble(words[i].substring(1));
-//                 double discount_value = price*(discount/100.0);
-//                 price -= discount_value;
-//                 words[i] = "$" + String.format("%.2f", price);
-//                 // System.out.println(price);
-//             } 
+        
+//         String ans="";
+//         String[]arr=sentence.split(" ");
+        
+//         for(int i=0;i<arr.length;i++)
+//         {
             
+//              if(arr[i].charAt(0)=='$' && isNumeric(arr[i].substring(1)))
+//             {
+//                   double dc=(double)(Double.parseDouble(arr[i].substring(1))*(discount/100.0));
+//                   double ap=(Double.parseDouble(arr[i].substring(1))-dc);
+//                   if(i==arr.length-1 && arr[arr.length-1].charAt(0)=='$')ans+="$"+String.format("%.2f", ap);
+//                  else ans+="$"+String.format("%.2f", ap)+" ";
+                 
+               
+//             }else{
+//                  if(i==arr.length-1)ans+=arr[i];
+//                  else ans+=arr[i]+" ";
+                 
+//              }
+           
 //         }
-        
-//         StringBuilder sb = new StringBuilder();
-//         String result;
-
-//         for (String s : words) {
-//             sb.append(s).append(' ');
-//         }
-//         result = sb.toString();
-        
-//         return result.substring(0,result.length()-1);
+//         return ans;
 //     }
-
 // }
+class Solution {
+public static boolean isNumeric(String str)
+    {
+        if(str.isEmpty()) return false;
+        
+        for (char c : str.toCharArray())
+        {
+            if (!Character.isDigit(c)) return false;
+        }
+        return true;
+    }
+    
+    
+    public String discountPrices(String sentence, int discount) {
+        String[] words = sentence.split(" ");
+        for(int i=0;i<words.length;i++){
+            if(words[i].charAt(0)=='$' &&  isNumeric(words[i].substring(1))){
+                // System.out.println(words[i]);
+                double price = Double.parseDouble(words[i].substring(1));
+                double discount_value = price*(discount/100.0);
+                price -= discount_value;
+                words[i] = "$" + String.format("%.2f", price);
+                // System.out.println(price);
+            } 
+            
+        }
+        
+        StringBuilder sb = new StringBuilder();
+        String result;
+
+        for (String s : words) {
+            sb.append(s).append(' ');
+        }
+        result = sb.toString();
+        
+        return result.substring(0,result.length()-1);
+    }
+
+}
