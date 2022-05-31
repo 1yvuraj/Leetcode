051522838d183fc2b691ea60d885f1c782f12556
YUVRAJ AGGARWAL <87929724+1yvuraj@users.noreply.github.com>
2022-05-31 22:37:35 +0530

    Time: 1 ms (74.16%), Space: 44.9 MB (53.39%) - LeetHub

--- 117-populating-next-right-pointers-in-each-node-ii/117-populating-next-right-pointers-in-each-node-ii.java
+++ 117-populating-next-right-pointers-in-each-node-ii/117-populating-next-right-pointers-in-each-node-ii.java
@@ -1,44 +1,39 @@
-/*
-// Definition for a Node.
-class Node {
-    public int val;
-    public Node left;
-    public Node right;
-    public Node next;
-
-    public Node() {}
-    
-    public Node(int _val) {
-        val = _val;
-    }
-
-    public Node(int _val, Node _left, Node _right, Node _next) {
-        val = _val;
-        left = _left;
-        right = _right;
-        next = _next;
-    }
-};
-*/
-
 class Solution {
    public Node connect(Node root) {
     Node cur = root;
+    Node head=null;
+    Node tail=null;
     while (cur != null) {
-        Node head = new Node();
-        Node child = head;
-        while (cur != null) {
-            if (cur.left != null) {
-                child.next = cur.left;
-                child = child.next;
-            }
-            if (cur.right != null) {
-                child.next = cur.right;
-                child = child.next;
-            }
-            cur = cur.next;
-        }
-        cur = head.next;
+       while(cur!=null)
+       {
+           if(cur.left!=null)
+           {
+               if(head==null)
+               {
+                   head=tail=cur.left;
+               }else {
+                   tail.next=cur.left;
+                   tail=cur.left;
+               }
+           }
+           if(cur.right!=null)
+           {
+               if(head==null)
+               {
+                   head=tail=cur.right;
+               }else {
+                   tail.next=cur.right;
+                   tail=cur.right;
+               }
+           }
+           cur=cur.next;
+          
+       }
+      cur=head;
+      tail=null;
+      head=null;
+      
+      
     }
     return root;
 }
