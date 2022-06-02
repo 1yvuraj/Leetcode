cf879633d92ddaec77cf2a6d1c9b1f7ae8e656b2
YUVRAJ AGGARWAL <87929724+1yvuraj@users.noreply.github.com>
2022-06-02 14:38:05 +0530

    Time: 1 ms (43.87%), Space: 41.9 MB (64.31%) - LeetHub

--- 144-binary-tree-preorder-traversal/144-binary-tree-preorder-traversal.java
+++ 144-binary-tree-preorder-traversal/144-binary-tree-preorder-traversal.java
@@ -1,27 +1,33 @@
 class Solution {
     public List<Integer> preorderTraversal(TreeNode root) {
-        TreeNode cur = root;
-        ArrayList<Integer> ans = new ArrayList<>();
-        while (cur != null) {
-            if (cur.left == null) {
-                ans.add(cur.val);
-                cur = cur.right;
-            } else {
-                TreeNode p = cur.left;  
-                while (p.right != null && p.right != cur) {
-                    p = p.right;
-                }
-                if (p.right == null) {
-                    ans.add(cur.val);
-                    p.right = cur;
-                    cur = cur.left;
-                }
-                if (p.right == cur) {
-                    p.right = null;
-                    cur = cur.right;
-                }
-            }
-        }
+       ArrayList<Integer>ans=new ArrayList<>();        
+       TreeNode cur=root;
+       while(cur!=null)
+       {
+           if(cur.left==null)
+           {
+               ans.add(cur.val);
+               cur=cur.right;
+           }else{
+               TreeNode p=cur.left;
+               while(p.right!=null && p.right!=cur){
+                   p=p.right;
+               }
+               if(p.right==null)
+               {
+                   p.right=cur;
+                   ans.add(cur.val);
+                   cur=cur.left;
+                   
+               }
+               if(p.right==cur)
+               {
+                   p.right=null;
+                   cur=cur.right;
+                   
+               }
+           }
+       }
         return ans;
     }
 }
