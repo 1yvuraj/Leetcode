3b7bee9bd5bd274a53e19e7aa097ce06bb7d2246
YUVRAJ AGGARWAL <87929724+1yvuraj@users.noreply.github.com>
2022-06-03 12:22:15 +0530

    Time: 0 ms (100.00%), Space: 42.2 MB (22.23%) - LeetHub

--- 951-flip-equivalent-binary-trees/951-flip-equivalent-binary-trees.java
+++ 951-flip-equivalent-binary-trees/951-flip-equivalent-binary-trees.java
@@ -5,14 +5,16 @@ class Solution {
     }
     public boolean help(TreeNode root1, TreeNode root2) {
         
-        if (  root1 == root2) {
+         if(root1 == null && root2 == null){
             return true;
-        }
-        if (root1==null || root2==null ||root1.val!=root2.val) {
+         }
+       else if (root1!=null && root2!=null &&root1.val==root2.val) {
+            return  help(root1.left, root2.left) && help(root1.right, root2.right) || help(root1.left, root2.right) && help(root1.right, root2.left);
+        }else{
             return false;
         }
         
-        boolean b = help(root1.left, root2.left) && help(root1.right, root2.right) || help(root1.left, root2.right) && help(root1.right, root2.left);
-        return b;
+        
+        //return b;
     }
 }
